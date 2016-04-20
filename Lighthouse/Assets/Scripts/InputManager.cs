c6467e150be34809635be6b27da1e0f25d01befb
SineR <patryk.kochanski@hotmail.com>
2016-04-20 16:54:16 +0200

    Init commit. + some starting tmp assets + singleton + input manager + light steering + base scene

---
+++ Lighthouse/Assets/Scripts/InputManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : Singleton<InputManager>
+{
+    public bool ThisFrameTouch, PreviousFrameTouch;
+    public Vector2 TouchPosition, PreviousTouchPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+#if UNITY_ANDROID && !UNITY_EDITOR
+	    PreviousFrameTouch = ThisFrameTouch;
+	    PreviousTouchPosition = TouchPosition;
+	    if (Input.touchCount > 0)
+	    {
+	        TouchPosition = Input.touches[0].position;
+	        ThisFrameTouch = true;
+	    }
+	    else
+	    {
+	        ThisFrameTouch = false;
+	    }
+#else
+        PreviousFrameTouch = ThisFrameTouch;
+        PreviousTouchPosition = TouchPosition;
+	    if (Input.GetMouseButton(0))
+	    {
+            TouchPosition = Input.mousePosition;
+            ThisFrameTouch = true;
+        }
+        else
+        {
+            ThisFrameTouch = false;
+        }
+#endif
+    }
+}
---
+++ Lighthouse/Assets/Scripts/LightSteering.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class LightSteering : MonoBehaviour
+{
+    private bool _targeted;
+    //private Vector3 Rotate
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    InputGetter();
+	}
+
+    private void InputGetter()
+    {
+        if (InputManager.Instance.ThisFrameTouch)
+        {
+            if (_targeted && InputManager.Instance.PreviousFrameTouch)
+            {
+                Vector2 prev = Camera.main.ScreenToWorldPoint(InputManager.Instance.PreviousTouchPosition);
+                Vector2 cur = Camera.main.ScreenToWorldPoint(InputManager.Instance.TouchPosition);
+
+                float angle = Vector2.Angle(prev, cur);
+
+                Vector3 cross = Vector3.Cross(prev,cur);
+                angle = cross.z > 0 ? angle : 360f - angle ;
+
+                gameObject.transform.Rotate(Vector3.forward, angle);
+            }
+            else
+            {
+                Ray ray = Camera.main.ScreenPointToRay(InputManager.Instance.TouchPosition);
+                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+                if (hit.collider != null)
+                {
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Light"))
+                    {
+                        _targeted = true;
+                    }
+                }
+                else
+                {
+                    _targeted = false;
+                }
+            }
+        }
+    }
+}
