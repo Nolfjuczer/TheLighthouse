ae302bc5518c15bb3f40c8fba7ac81a26c9e1fc3
filipw <wrobelfilip@gmail.com>
2016-06-15 22:28:14 +0200

    [+] GameLord class

---
+++ Lighthouse/Assets/Scripts/Multiscene/GameLord.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Multiscene
+{
+	public class GameLord : MonoBehaviour
+	{
+		#region Variables
+
+		public enum GameState
+		{
+			GS_MENU = 0,
+			GS_LOADING = 1,
+			GS_GAME = 2
+		}
+
+		private GameState _currentGameState = GameState.GS_MENU;
+		public GameState CurrentGameState { get { return _currentGameState; } }
+
+		#endregion Variables
+
+		#region Monobehaviour Methods
+		void Start()
+		{
+
+		}
+
+		void Update()
+		{
+
+		}
+		#endregion Monobehaviour Methods
+
+		#region Methods
+
+		#endregion Methods
+	}
+}
